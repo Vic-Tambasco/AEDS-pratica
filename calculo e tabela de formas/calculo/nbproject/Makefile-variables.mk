3c51c008e891d7dbd3568696cf52ba7c4a01eba3
Victoria Tambasco <victoriaa.tambasco@gmail.com>
2025-06-24 16:53:58 -0300

    Add files via upload

---
+++ calculo e tabela de formas/calculo/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=calculo.formas
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/calculo.formas
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=calculo.formas.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/calculo.formas.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=calculo.formas
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/calculo.formas
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=calculo.formas.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/calculo.formas.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
