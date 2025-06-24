1c8a1603a8c2024f5e03df55d0e11aa824836825
Victoria Tambasco <victoriaa.tambasco@gmail.com>
2025-06-24 18:25:32 -0300

    Add files via upload

---
+++ validacao de senha/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=valida_ao.de.senha.com.tentativas.limitadas
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/valida_ao.de.senha.com.tentativas.limitadas
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=validaçao.de.senha.com.tentativas.limitadas.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/validaçao.de.senha.com.tentativas.limitadas.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=valida_ao.de.senha.com.tentativas.limitadas
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/valida_ao.de.senha.com.tentativas.limitadas
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=validaçao.de.senha.com.tentativas.limitadas.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/validaçao.de.senha.com.tentativas.limitadas.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
